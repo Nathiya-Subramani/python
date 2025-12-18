9ff5c11d37af44aaf027baa0f5563d585c7667d0
Nathiya-Subramani <nathiyasubramani25@gmail.com>
2025-12-18 18:42:46 +0530

    day 4 if else homework

---
+++ homework_day04.py
@@ -0,0 +1,41 @@
+# mark = 85
+# if mark > 90:
+#     grade = "A"
+# elif mark > 80:
+#     grade = "B"
+# else:
+#     grade = "C"
+# print("Mark:", mark)
+# print("Grade:", grade)   
+
+# salary = 900000      # annual salary
+# if  salary >= 400000 and salary < 800000:
+#   tax = "5%"
+# elif salary >= 800000 and salary < 1200000:
+#   tax = "10%"
+# elif salary >= 1200000 and salary < 1600000:
+#   tax = "15%" 
+# elif salary >= 1600000 and salary < 2000000:
+#   tax = "20%"
+# elif salary >= 2000000 and salary <2500000:
+#   tax = "25%"
+# else:
+#   tax = "30%"
+# print("Salary:", salary)
+# print("Tax rate:", tax)
+# gold = 10
+metal = "Silver"      #gold rate =13528 #silver=196 #platinum=4798 
+metal = metal.lower()
+print(metal)
+if metal=="gold":
+   rate = 13528
+elif metal=="silver":
+   rate = 196
+elif metal=="platinum":
+   rate = 4798
+else :
+   rate = 0
+print("metal price:",metal)
+print("rate of metal:",rate)
+
+    
