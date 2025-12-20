1ea4dc983b679a59cecee055c446811272937cf8
Nathiya-Subramani <nathiyasubramani25@gmail.com>
2025-12-20 17:46:13 +0530

    hw_06 functions.py

---
+++ hwday_06 functions.py
@@ -0,0 +1,24 @@
+def add(x, y):
+    print(x + y)
+
+add(5, 4)
+def sub(x, y):
+    print(x - y)
+     
+sub(10,5)
+def mul(x, y):
+    print(x * y)
+
+mul(8,8)
+def div(x, y):
+    print(x / y)
+
+div(22,9)
+def floor_div (x , y):
+    print(x // y)
+
+floor_div(9,8)
+def can_vote(age):
+    print(age >= 18)
+
+can_vote(17)
