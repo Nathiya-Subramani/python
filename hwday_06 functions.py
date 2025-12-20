4f0b8e16a59b094bc9112a4b86389e8ed6aa41a7
Nathiya-Subramani <nathiyasubramani25@gmail.com>
2025-12-20 18:31:53 +0530

    Update hwday_06 functions.py

--- hwday_06 functions.py
+++ hwday_06 functions.py
@@ -1,24 +1,45 @@
-def add(x, y):
-    print(x + y)
+# def add(x, y):
+#     print(x + y)
 
-add(5, 4)
-def sub(x, y):
-    print(x - y)
+# add(5, 4)
+
+# def sub(x, y):
+#     print(x - y)
      
-sub(10,5)
-def mul(x, y):
-    print(x * y)
+# sub(10,5)
 
-mul(8,8)
-def div(x, y):
-    print(x / y)
+# def mul(x, y):
+#     print(x * y)
 
-div(22,9)
-def floor_div (x , y):
-    print(x // y)
+# mul(8,8)
 
-floor_div(9,8)
-def can_vote(age):
-    print(age >= 18)
+# def div(x, y):
+#     print(x / y)
 
-can_vote(17)
+# div(22,9)
+
+# def floor_div (x , y):
+#     print(x // y)
+
+# floor_div(9,8)
+
+# def can_vote(age):
+#     print(age >= 18)
+
+# can_vote(17)
+
+def is_minor(age):
+    print(age <= 18)
+
+is_minor(9)
+is_minor(25)
+
+def is_centum(mark):
+    print("is_centum", mark == 100)
+
+is_centum(100)
+
+def has_fever(temp):
+    print("has_fever",temp > 100)
+
+has_fever(102)
