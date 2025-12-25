e2f6f1e79c0ba7e621dcd0422eed62568927ac65
Nathiya-Subramani <nathiyasubramani25@gmail.com>
2025-12-25 09:22:26 +0530

    loop.py

---
+++ loop.py
@@ -0,0 +1,68 @@
+# # # def add (a,b):
+# # #     return a + b
+
+
+# # # result = add(10,20)
+# # # print(result)
+
+# # # def mul (a,b):
+# # #     return a * b
+
+# # # result = mul(5,4)
+# # # print(result)
+
+# # # def max (a,b):      
+# # #     if a > b:
+# # #       return a
+# # #     else:
+# # #       return b
+    
+# # # result = max (10,20)
+# # # print(result)
+
+# # # def min (x,y):
+# # #    if x < y:
+# # #       return x
+# # #    else:
+# # #       return y
+   
+# # # result = min (40,55)
+# # # print(result)
+ 
+# # # x = 1
+# # # while x <= 16:
+# # #     print(x)
+# # #     x = x + 1
+
+# # x = 1
+# # while x <= 10:
+# #     print(x)
+# #     x = x + 1
+
+# # 2,4,6,8,10,upto 20
+# # start 2
+# # end 20
+# # step 2
+# # x = 2             
+# # while x <= 20 :
+# #     print(x)
+# #     x = x + 2
+
+# # 5-50,3-30
+
+# # 3,6,9,12,15,18,upto 30
+# start 3
+# end 30
+# step 3 
+
+# x = 3
+# while x <= 30 :
+#     print(x)
+#     x = x + 3 
+
+#5,10,15,20,upto 50
+
+x = 5
+while x <= 50:
+    print(x)
+    x = x + 5
