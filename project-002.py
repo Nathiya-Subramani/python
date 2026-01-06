09efa0f7475dea229a4eb4b82794bda20f88eb76
Nathiya-Subramani <nathiyasubramani25@gmail.com>
2026-01-06 18:45:33 +0530

    project-002.py

--- project-002.py
+++ project-002.py
@@ -7,25 +7,15 @@
 # calculate percentage 
 # calculate cutoff (maths,physics,chemistry)
 
+# get marks
 
-# get  marks
-
-# tamil = int(input("enter tamil mark:"))
-# english = int(input("enter english mark:"))
-# maths = int(input("enter maths mark:"))
-# chemistry = int(input("enter chemistry mark:"))
-# physics = int(input("enter physics mark:"))
-
-tamil = 90
-english = 90
-maths = 99
+tamil = 45
+english = 30
+maths = 35
 chemistry = 96
 physics = 100
 
 # calaculate total
-
-# total = tamil + english + maths + chemistry + physics
-# print("Total marks:",total) 
 
 def calculate_total(ta,en,ma,ch,ph):
     total = ta + en + ma + ch + ph
@@ -36,44 +26,76 @@ print("total marks:",total)
 
 # calculate percentage
 
-percentage = total / 500 * 100
-print("percentage of a student:",percentage)
+def calculate_percentage(total):
+        return(total/500)*100
+percentage = calculate_percentage(total)
+print("percentage:",percentage)
 
 # pass or fail
 
-if tamil >= 35 and english >= 35 and maths >= 35 and chemistry >= 35 and physics >= 35:
-    print("Result : PASS")
-else:
-    print("Result : FAIL")
+def pass_or_fail(ta,en,ma,ch,ph):
+    if tamil >= 35 and english >= 35 and maths >= 35 and chemistry >= 35 and physics >= 35:
+        return "PASS"
+    else:
+        return "FAIL"
+    
+result = pass_or_fail(tamil,english,maths,chemistry,physics)
+print("pass_or_fail:",result)
 
 # check grade 
 
-if percentage >= 90:
-    print("grade:A")
-elif percentage >= 75:
-    print("grade:b")
-elif percentage >= 60:
-    print("grade:c")
-
-else:
-    print("grade:d")
-
+def calculate_grade(percentage):
+     if percentage >= 90:
+          return "a"
+     elif percentage >= 75:
+          return "b"
+     elif percentage >= 60:
+          return "c"
+     else:
+          return "d"
+     
+grade = calculate_grade(percentage)
+print("grade of a student:",grade)
+     
 # top score
 
-top_score = max(tamil,english,maths,chemistry,physics)
-print("Top score of a student:",top_score)
+def top_score(ta,en,ma,ch,ph):
+     return max (ta, en , ma , ch, ph)
+
+top = top_score(tamil,english,maths,chemistry,physics)
+print("top score f a student:",top)
 
 # check centum
+def check_centum(ta,en,ma,ch,ph):
+     if tamil == 100:
+          print ("tamil centum")
+     if english == 100:
+          print ("english centum")
+     if maths == 100:
+          print ("maths centum")
+     if chemistry == 100:
+          print ("chemistry centum")
+     if physics == 100:
+          print ("physics centum")
+          
+check_centum(tamil,english,maths,chemistry,physics)
 
-if tamil == 100 or english == 100 or maths == 100 or chemistry == 100 or physics == 100:
-    print("centum scored subject")
-else:
-    print("no centum scored")
+def check_mark(mark , subject):
+     if mark == 100:
+          print("centum in:",subject)
 
-#calculate cutoff
+check_mark (tamil,"Tamil")
+check_mark (100,"Maths")
 
-cutoff_marks = maths + (chemistry/2) +(physics/2)
-print("cutoff (M+C+p):",cutoff_marks) 
+#calculate cutoff 
+
+def calculate_cutoff(ma, ph, ch):
+    return ma + ph + ch
+
+cutoff = calculate_cutoff(maths, physics, chemistry)
+print("Cutoff:", cutoff)
+
+
 
 #h/w
 # convert all into function
