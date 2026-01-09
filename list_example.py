3f565307d5b9264b361c44a467cb1c580fa46858
Nathiya-Subramani <nathiyasubramani25@gmail.com>
2026-01-09 17:34:20 +0530

    list_hw.py

--- list_example.py
+++ list_example.py
@@ -1,9 +1,9 @@
 # numbers = [1,2,3,4,5]
 # print(numbers)
 
-numbers = [1,2,3,4,5]
-for i in numbers:
-    print(i)
+# numbers = [1,2,3,4,5]
+# for i in numbers:
+#     print(i)
 
 # student = ["nathiya","nandhini","poovarsi"]
 # print(student)
@@ -14,4 +14,25 @@ for i in numbers:
 # hw
 # total
 # max score
-# size of the list
+# size of the list  
+
+# total
+marks = [40,35,90,77,86]
+total = sum(marks)
+print("total:",total)
+
+# max_score
+max_score = max(marks)
+print("max_score:",max_score )
+
+# size
+size = len(marks)
+print("size of list:",size)
+
+# allover
+marks = [40,35,90,77,86]
+
+print("total:",sum(marks))
+print("max_score:",max(marks))
+print("size of the list:",len(marks))
+
